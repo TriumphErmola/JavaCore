03a37723de7c079d6ba1e9d8a98447428bdb0c9c
Andrey <xramovic@yandex.ru>
2022-03-07 16:15:42 +0600

    JavaCore.ZaurTregulov

---
+++ src/main/java/JavaCore/ArrayListLesson20/Test7.java
@@ -0,0 +1,17 @@
+package JavaCore.ArrayListLesson20;
+
+import java.util.ArrayList;
+
+public class Test7 {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+        students.add(1,"Petrov");
+        students.add(2,"Petrov");
+        students.add(3,"Petrov");
+        students.add(4,"Petrov");
+        students.remove(2);
+        for(String s : students){
+            System.out.println(s);
+        }
+    }
+}
