a040ff9c9ecc55d2b24a517e545a90c84fdb1e9e
Andrey <xramovic@yandex.ru>
2022-03-12 16:36:19 +0600

    JavaCore.ZaurTregulov

---
+++ src/main/java/JavaCore/Lesson23/Test2.java
@@ -0,0 +1,32 @@
+package JavaCore.Lesson23;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Mauss a = new Mauss();
+        a.showInfOAboutAnimal();
+        a.showInfOAboutMouse();
+    }
+
+}
+
+class Animall{
+     String showName(){
+        return "some Animall";
+    }
+
+    void showInfOAboutAnimal(){
+        System.out.println("Name of animal : "+ showName());
+    }
+}
+
+class Mauss extends Animall{
+
+    void showInfOAboutMouse(){
+        System.out.println("Name of mouse : "+ showName());
+    }
+
+     String showName(){
+        return "Jerry";
+    }
+
+}
