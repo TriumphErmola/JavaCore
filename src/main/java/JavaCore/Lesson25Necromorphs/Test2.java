17a3f8d4a4a0290333381c40a757838d4b64b632
Andrey <xramovic@yandex.ru>
2022-03-20 16:47:52 +0600

    JavaCore.ZaurTregulov

---
+++ src/main/java/JavaCore/Lesson25Necromorphs/Test2.java
@@ -0,0 +1,37 @@
+package JavaCore.Lesson25Necromorphs;
+
+public class Test2 {
+    public static void main(String[] args) {
+        JumpAble j = new Human();
+        Man m = new Man();
+        Car c = new Car();
+        if (j instanceof JumpAble) {
+            System.out.println("J is jumpable");
+        }
+        if (m instanceof Human) {
+            System.out.println("m is Human");
+        }
+//        if (c instanceof Human) {
+//            System.out.println("m is Human");
+//        }
+        if (c instanceof JumpAble) {
+            System.out.println("J is jumpable");
+        }
+    }
+}
+
+interface JumpAble {
+
+}
+
+class Human implements JumpAble {
+
+}
+
+class Man extends Human {
+
+}
+
+class Car {
+
+}
---
+++ src/main/java/JavaCore/Lesson25Necromorphs/Test3.java
@@ -0,0 +1,65 @@
+package JavaCore.Lesson25Necromorphs;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Employees emp1 = new Stomatolog();
+        Employees emp2 = new Mentor();
+        Employees emp3 = new Voditel();
+        Stomatolog st1 = (Stomatolog) emp1;
+        Voditel vod1 = (Voditel) emp3;
+        System.out.println(st1.spesialization);
+        System.out.println(vod1.carName);
+        System.out.println(((Stomatolog) emp1).spesialization);
+
+        HelpAble h1 = new Stomatolog();
+        h1.help();
+        System.out.println(((Stomatolog) h1).spesialization);
+        ((Stomatolog) h1).lechit();
+
+    }
+}
+
+
+class Employees {
+    double salary = 100;
+    String name = "Andrey";
+    int age;
+    int experience;
+
+    void eat() {
+        System.out.println("Employees kuchac");
+    }
+
+    void sleep() {
+        System.out.println("Employees otdihaet");
+    }
+}
+
+class Stomatolog extends Employees implements HelpAble {
+    String spesialization = "dantist";
+
+    void lechit() {
+        System.out.println("Stomatolog lechit");
+    }
+
+    @Override
+    public void help() {
+        System.out.println("Stomatolog okazivaet pomosh");
+    }
+}
+
+class Mentor extends Employees {
+    int students;
+
+    void uchit() {
+        System.out.println("Mentor uchit");
+    }
+}
+
+class Voditel extends Employees {
+    String carName = "PANAMERA";
+
+    void voditCar() {
+        System.out.println("Voditel vodit");
+    }
+}
