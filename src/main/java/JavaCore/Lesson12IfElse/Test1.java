7b51dfc7c89f13292ae35d099e5e6cfdad1f77a4
Andrey <xramovic@yandex.ru>
2022-04-02 17:01:44 +0600

    JavaCore.ZaurTregulov

---
+++ src/main/java/JavaCore/Lesson12IfElse/Test1.java
@@ -0,0 +1,13 @@
+package JavaCore.Lesson12IfElse;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        int a = 25;
+        if (a < 20) {
+            System.out.println("a меньше 20");
+        } else {
+            System.out.println("a больше 20");
+        }
+    }
+}
