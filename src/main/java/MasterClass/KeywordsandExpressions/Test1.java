2ff64a683a1a02fa941f1ac556b43b1ffd00b22c
Andrey <xramovic@yandex.ru>
2022-04-13 20:59:35 +0600

    JavaCore

--- src/main/java/MasterClass/KeywordsandExpressions/Test1.java
+++ src/main/java/MasterClass/KeywordsandExpressions/Test1.java
@@ -1,4 +1,12 @@
 package MasterClass.KeywordsandExpressions;
 
 public class Test1 {
+    public static void main(String[] args) {
+
+        double kilometers = (100 * 1.6093444);
+        int hightScore = 54;
+        if (hightScore == 100) {
+            System.out.println("this is an expression");
+        }
+    }
 }
