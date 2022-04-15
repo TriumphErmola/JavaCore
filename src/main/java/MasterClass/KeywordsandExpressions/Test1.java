e9c3139892ff001051e6604e345fea79053c1b78
Andrey <xramovic@yandex.ru>
2022-04-15 21:51:01 +0600

    JavaCore

--- src/main/java/MasterClass/KeywordsandExpressions/Test1.java
+++ src/main/java/MasterClass/KeywordsandExpressions/Test1.java
@@ -2,11 +2,18 @@ package MasterClass.KeywordsandExpressions;
 
 public class Test1 {
     public static void main(String[] args) {
+        calculatedScore(false, 250, 21, 1500);
+    }
 
-        double kilometers = (100 * 1.6093444);
-        int hightScore = 54;
-        if (hightScore == 100) {
-            System.out.println("this is an expression");
+
+    public static int calculatedScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score : " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
     }
 }
