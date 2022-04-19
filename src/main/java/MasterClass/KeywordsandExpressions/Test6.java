f464a4e6274a39adc2ece4cb420b8c345f28ec84
Andrey <xramovic@yandex.ru>
2022-04-19 21:12:51 +0600

    JavaCore.MasterClass.TimBuchanka

--- src/main/java/MasterClass/KeywordsandExpressions/Test6.java
+++ src/main/java/MasterClass/KeywordsandExpressions/Test6.java
@@ -2,8 +2,8 @@ package MasterClass.KeywordsandExpressions;
 
 public class Test6 {
     public static void main(String[] args) {
-        System.out.println(getDurationString(65, 45));
-        System.out.println(getDurationString(36000));
+
+        System.out.println(getDurationString(7200));
     }
 
     private static String getDurationString(long minutes, long seconds) {
