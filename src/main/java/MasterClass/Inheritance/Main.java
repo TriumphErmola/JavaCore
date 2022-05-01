4dd37377d814c4c1ef89767b12e51c8f6cd2c15e
Andrey <xramovic@yandex.ru>
2022-05-01 12:03:44 +0600

    JavaCore.MasterClass.TimBuchanka.Classes

--- src/main/java/MasterClass/Inheritance/Main.java
+++ src/main/java/MasterClass/Inheritance/Main.java
@@ -2,11 +2,10 @@ package MasterClass.Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal1 = new Animal("Cat", 1, 1, 1, 10);
 
-        Dog dog = new Dog("Bobik", 2, 15, 2, 4, 2, 3, "gav-gav");
-//        dog.eat();
-        dog.walk();
-//        dog.run();
+    Porsche porsche = new Porsche(72);
+    porsche.handSteering(15);
+    porsche.accelerate(1);
+
     }
 }
