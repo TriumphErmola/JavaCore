38419ae173496ba34cba78168ee72ba2c18e91ad
Andrey <xramovic@yandex.ru>
2022-05-05 20:49:15 +0600

    JavaCore.MasterClass.TimBuchanka.Classes

--- src/main/java/MasterClass/Array/ScannerAndArrayAVG.java
+++ src/main/java/MasterClass/Array/ScannerAndArrayAVG.java
@@ -7,7 +7,7 @@ public class ScannerAndArrayAVG {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] myIntegers = getIntegers(6);
+        int[] myIntegers = getInteger(6);
         for (int i = 0; i < myIntegers.length; i++) {
             System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
@@ -15,7 +15,7 @@ public class ScannerAndArrayAVG {
 
     }
 
-    public static int[] getIntegers(int number) {
+    public static int[] getInteger(int number) {
         System.out.println("Enter " + number + " integer  values.\r");
         int[] values = new int[number];
 
---
+++ src/main/java/MasterClass/Array/Test1.java
@@ -0,0 +1,11 @@
+package MasterClass.Array;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int[] myArray = {1, 4, 6, 3, 9};
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+    }
+}
+
