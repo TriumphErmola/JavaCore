49508606dd09a84fecfc3fb630c947fa6ca82712
Andrey <xramovic@yandex.ru>
2022-05-15 18:07:32 +0600

    JavaCore.MasterClass.TimBuchanka.Classes

--- src/main/java/MasterClass/Collections/LinkedList/Demo.java
+++ src/main/java/MasterClass/Collections/LinkedList/Demo.java
@@ -2,6 +2,7 @@ package MasterClass.Collections.LinkedList;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
@@ -26,6 +27,23 @@ public class Demo {
             System.out.println("Now visiting " + iterator.next());
         }
         System.out.println("=======");
+    }
 
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext()) {
+            int comparison = listIterator.next().compareTo(newCity);
+            if (comparison == 0) {//равны не добавляем
+                System.out.println(newCity + " is already included as s destination");
+                return false;
+            } else if (comparison > 0) {//новый пунтк поэтому добавляем
+                listIterator.previous();
+                listIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+
+            }
+        }
+        listIterator.add(newCity);
     }
 }
