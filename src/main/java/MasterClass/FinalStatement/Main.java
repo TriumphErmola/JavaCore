87f3c4622780f78724cf6e9b84a6b74a01dfd492
Andrey <xramovic@yandex.ru>
2022-06-06 16:57:05 +0500

    final theme and crypted password

--- src/main/java/MasterClass/FinalStatement/Main.java
+++ src/main/java/MasterClass/FinalStatement/Main.java
@@ -1,4 +1,29 @@
 package MasterClass.FinalStatement;
 
-public class Main {
+public class Main{
+
+    public static void main(String[] args) {
+
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        System.out.println(Math.PI);
+        System.out.println("**********************************");
+
+        int pw = 1525;
+        Password password = new Password(pw);
+        password.storePassword();
+
+        password.letMeIn(1);
+        password.letMeIn(41341);
+        password.letMeIn(5121);
+        password.letMeIn(-1512412);
+        password.letMeIn(1525);
+
+    }
 }
