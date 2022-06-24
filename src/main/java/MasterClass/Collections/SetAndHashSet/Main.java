ccb4744868ad0300f6d8c6ff23e1fe512d019471
Andrey <xramovic@yandex.ru>
2022-06-24 10:22:58 +0500

    Set

---
+++ src/main/java/MasterClass/Collections/SetAndHashSet/Main.java
@@ -0,0 +1,57 @@
+package MasterClass.Collections.SetAndHashSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    private static Map<String, NebesnoeTelo> solarnayaSystema = new HashMap<>();
+    private static Set<NebesnoeTelo> planeti = new HashSet();
+
+    public static void main(String[] args) {
+
+        NebesnoeTelo telo = new NebesnoeTelo("Merkuriy", 88);
+        solarnayaSystema.put(telo.getNameOfObject(), telo);
+        planeti.add(telo);
+
+        telo = new NebesnoeTelo("Venera", 225);
+        solarnayaSystema.put(telo.getNameOfObject(), telo);
+        planeti.add(telo);
+
+        telo = new NebesnoeTelo("Earth", 365);
+        solarnayaSystema.put(telo.getNameOfObject(), telo);
+        planeti.add(telo);
+
+        NebesnoeTelo sputnik1 = new NebesnoeTelo("Luna", 27);
+        solarnayaSystema.put(sputnik1.getNameOfObject(), sputnik1);
+        telo.addLuna(sputnik1);
+
+        telo = new NebesnoeTelo("Mars", 687);
+        solarnayaSystema.put(telo.getNameOfObject(), telo);
+        planeti.add(telo);
+
+        sputnik1 = new NebesnoeTelo("Deimos", 1.3);
+        solarnayaSystema.put(sputnik1.getNameOfObject(), sputnik1);
+        telo.addLuna(sputnik1); // temp is still Mars
+
+        sputnik1 = new NebesnoeTelo("Phobos", 0.3);
+        solarnayaSystema.put(sputnik1.getNameOfObject(), sputnik1);
+        telo.addLuna(sputnik1);
+
+        System.out.println("Planeti");
+        for (NebesnoeTelo planeti : planeti) {
+            System.out.println("\t" + planeti.getNameOfObject());
+        }
+
+        NebesnoeTelo teloTest = solarnayaSystema.get("Mars");
+        System.out.println("Luni of " + teloTest.getNameOfObject());
+        for(NebesnoeTelo sputnikiThisObject : teloTest.getSputniki()){
+            System.out.println("\t" + sputnikiThisObject.getNameOfObject());
+
+        }
+
+
+    }
+}
---
+++ src/main/java/MasterClass/Collections/SetAndHashSet/NebesnoeTelo.java
@@ -0,0 +1,34 @@
+package MasterClass.Collections.SetAndHashSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class NebesnoeTelo {
+
+    private final String nameOfObject;
+    private final double orbitalnoeVremy;
+    private final Set<NebesnoeTelo> sputniki;
+
+    public NebesnoeTelo(String nameOfObject, double orbitalnoeVremy) {
+        this.nameOfObject = nameOfObject;
+        this.orbitalnoeVremy = orbitalnoeVremy;
+        this.sputniki = new HashSet<>();
+    }
+
+    public String getNameOfObject() {
+        return nameOfObject;
+    }
+
+    public double getOrbitalnoeVremy() {
+        return orbitalnoeVremy;
+    }
+
+    public Set<NebesnoeTelo> getSputniki() {
+        return new HashSet<>(this.sputniki);
+    }
+
+    public boolean addLuna(NebesnoeTelo luna) {
+        return this.sputniki.add(luna);
+    }
+
+}
